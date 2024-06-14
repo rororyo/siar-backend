c15db71d7322bfa80b2f52833f157259bdffb4d9
Nathaniel Ryo <ryonathaniel05@gmail.com>
2024-06-14 20:28:52 +0700

    added routes

--- routes/homepage.js
+++ routes/homepage.js
@@ -104,7 +104,7 @@ homepage.get("/api/article/:id", async (req, res) => {
   }
 })
 
-homepage.post('/api/nearbyPlaces', async (req, res) => {
+homepage.post('/api/nearby-places', async (req, res) => {
   const { latitude, longitude } = req.body;
   const client = req.dbClient;
   const query = `
@@ -118,17 +118,17 @@ homepage.post('/api/nearbyPlaces', async (req, res) => {
     AND earth_distance(
         ll_to_earth(lat, long),
         ll_to_earth($1, $2)
-      ) < 20000
+      ) < 2000 
     ORDER BY rating DESC
     LIMIT 5;
   `;
 
   try {
     const result = await client.query(query, [latitude, longitude]);
-    res.json(result.rows);
+    res.status(200).json({ places: result.rows });
   } catch (err) {
     console.error('Error executing query', err.stack);
-    res.status(500).send('Error fetching nearby places');
+    res.status(500).json({ message: err.message });
   }
 });
 
